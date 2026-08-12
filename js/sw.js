fa0cd125a57f9ef22c552be53792690159aefc75
anthony <tkachanton1207@gmail.com>
2026-08-12 12:29:23 +0200

    fixe sw for netlify

--- js/sw.js
+++ js/sw.js
@@ -1,29 +1,33 @@
-const CACHE_NAME = "glaze-v1"
+const CACHE_NAME = "glaze-v1";
 const FILES_TO_CACHE = [
-  "/",
-  "/index.html",
-  "/styles/styles.css",
-  "/js/scripts.js",
-  "/img/logo/icon192.png",
-  "/img/logo/icon512.png"
-]
+  "./",
+  "./index.html",
+  "./styles/styles.css",
+  "./js/scripts.js",
+  "./img/logo/icon192.png",
+  "./img/logo/icon512.png",
+];
 
 self.addEventListener("install", (e) => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
-  )
-})
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE)),
+  );
+});
 
 self.addEventListener("activate", (e) => {
   e.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
-    )
-  )
-})
+    caches
+      .keys()
+      .then((keys) =>
+        Promise.all(
+          keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)),
+        ),
+      ),
+  );
+});
 
 self.addEventListener("fetch", (e) => {
   e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
-  )
-})
+    caches.match(e.request).then((response) => response || fetch(e.request)),
+  );
+});
