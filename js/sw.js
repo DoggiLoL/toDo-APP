245de2fa1dca2af0d8a336c3799f46fcf142eed5
anthony <tkachanton1207@gmail.com>
2026-08-12 12:30:30 +0200

    one more fix

--- js/sw.js
+++ js/sw.js
@@ -4,8 +4,8 @@ const FILES_TO_CACHE = [
   "./index.html",
   "./styles/styles.css",
   "./js/scripts.js",
-  "./img/logo/icon192.png",
-  "./img/logo/icon512.png",
+  "./img/logo/glaze_logo_app.png",
+  "./img/logo/glaze_logo_web_store.png",
 ];
 
 self.addEventListener("install", (e) => {
